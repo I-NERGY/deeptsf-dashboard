eec41cf9cafefe8f3146de144e64540e7ce61952
gkorbakis <gkorbakis@epu.ntua.gr>
2022-06-24 18:12:38 +0300

    LoadForecast.js some fixes

--- src/pages/LoadForecast.js
+++ src/pages/LoadForecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
 import Link from '@mui/material/Link';
@@ -10,7 +10,11 @@ import IconButton from '@mui/material/IconButton';
 import MuiAlert from '@mui/material/Alert';
 import Snackbar from '@mui/material/Snackbar';
 import Checkbox from '@mui/material/Checkbox';
-
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Grid from '@mui/material/Grid';
 
 import UploadFileOutlinedIcon from '@mui/icons-material/UploadFileOutlined';
 import ModelTrainingIcon from '@mui/icons-material/ModelTraining';
@@ -36,11 +40,19 @@ const breadcrumbs = [<Link fontSize={'20px'} underline="hover" key="1" color="in
 const LoadForecast = () => {
     const [newFile, setNewFile] = useState()
     const [dayFirst, setDayFirst] = useState(false)
+    const [models, setModels] = useState([])
+    const [model, setModel] = useState('')
 
     const [loading, setLoading] = useState(false)
     const [newFileSuccess, setNewFileSuccess] = useState(false)
     const [newFileFailure, setNewFileFailure] = useState(false)
     const [newFileSnackbar, setNewFileSnackbar] = useState(false)
+
+    useEffect(() => {
+        axios.get('/models/get_model_names')
+            .then(response => setModels(response.data.models))
+            .catch(error => console.log(error))
+    })
 
     const handleAddNewFile = file => {
         setNewFile(file)
@@ -98,58 +110,87 @@ const LoadForecast = () => {
     return (<div>
         <Breadcrumb breadcrumbs={breadcrumbs} welcome_msg={'Welcome to I-NERGY Load Forecasting'}/>
 
-        <Container maxWidth={'xl'} sx={{mt: 5}}>
-            <Stack direction="row" spacing={2} sx={{alignItems: 'center'}}>
-                <input
-                    accept=".csv"
-                    style={{display: 'none'}}
-                    id="raised-button-file"
-                    type="file"
-                    onChange={(event) => handleAddNewFile(event.target.files[0])}
-                />
-
-                <label htmlFor="raised-button-file">
-                    <IconButton component={'span'} size={'large'}>
-                        <UploadFileOutlinedIcon fontSize="large" sx={{width: '80px', height: '80px'}}/>
-                    </IconButton>
-                </label>
-                <Typography variant={'h5'} color={'inherit'} sx={{width: '100%'}}>Upload your .csv file</Typography>
-                {newFile && <Typography variant={'h5'} color={'inherit'} align={'right'} sx={{width: '100%'}}>Chosen
-                    file: <Typography fontWeight={'bold'}
-                                      color={'secondary'}>{newFile.name}</Typography></Typography>}
-            </Stack>
-
-            <Stack direction="row" spacing={2} sx={{alignItems: 'center', mb: 2}}>
-                {newFile && <>
-                    <Typography sx={{ml: 'auto'}} variant={'h6'}>Day First</Typography>
-                    <Checkbox
-                        value={dayFirst}
-                        onChange={() => setDayFirst(!dayFirst)}
-                        sx={{'& .MuiSvgIcon-root': {fontSize: 28}}}
+        <Container maxWidth={'xl'} sx={{my: 5}}>
+            <Grid container spacing={2} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                <Grid item xs={12} md={6}>
+                    <input
+                        accept=".csv"
+                        style={{display: 'none'}}
+                        id="raised-button-file"
+                        type="file"
+                        onChange={(event) => handleAddNewFile(event.target.files[0])}
                     />
-                </>}
-            </Stack>
 
-            <Stack direction="row" spacing={2} sx={{alignItems: 'center'}}>
-                {newFile && <Button variant={'contained'} component={'span'} size={'large'} color={'success'}
-                                    sx={{ml: 'auto'}}
-                                    endIcon={<UploadFileOutlinedIcon/>} onClick={handleUploadFile}>
-                    Upload file
-                </Button>}
-            </Stack>
+                    <Stack direction="row" spacing={2} sx={{alignItems: 'center'}}>
+                        <label htmlFor="raised-button-file">
+                            <IconButton component={'span'} size={'large'}>
+                                <UploadFileOutlinedIcon fontSize="large" sx={{width: '80px', height: '80px', border: '1px solid red'}}/>
+                            </IconButton>
+                        </label>
+                        <Typography variant={'h5'} color={'inherit'} sx={{width: '100%'}}>
+                            Upload your .csv file
+                        </Typography>
+                    </Stack>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                    {newFile && <Typography variant={'h5'} color={'inherit'} align={'right'} sx={{width: '100%'}}>Chosen
+                        file: <Typography fontWeight={'bold'}
+                                          color={'secondary'}>{newFile.name}</Typography></Typography>}
+                    <Stack direction="row" spacing={2} sx={{alignItems: 'center', mb: 2}}>
+                        {newFile && <>
+                            <Typography sx={{ml: 'auto'}} variant={'h6'}>Day First</Typography>
+                            <Checkbox
+                                value={dayFirst}
+                                onChange={() => setDayFirst(!dayFirst)}
+                                sx={{'& .MuiSvgIcon-root': {fontSize: 28}}}
+                            />
+                        </>}
+                    </Stack>
+                    <Stack direction="row" spacing={2} sx={{alignItems: 'center'}}>
+                        {newFile && <Button variant={'contained'} component={'span'} size={'large'} color={'success'}
+                                            sx={{ml: 'auto'}}
+                                            endIcon={<UploadFileOutlinedIcon/>} onClick={handleUploadFile}>
+                            Upload file
+                        </Button>}
+                    </Stack>
+                </Grid>
+            </Grid>
         </Container>
         <hr/>
 
-        <Container maxWidth={'xl'} sx={{mt: 5}}>
-            <Stack direction="row" spacing={2} sx={{alignItems: 'center'}}>
-                <label htmlFor="raised-button-file">
-                    <IconButton component={'span'} size={'large'}>
-                        <ModelTrainingIcon fontSize="large" sx={{width: '80px', height: '80px', ml: 2}}/>
-                    </IconButton>
-                </label>
-                <Typography variant={'h5'} color={'inherit'} sx={{width: '100%'}}>Choose a model</Typography>
-            </Stack>
+        <Container maxWidth={'xl'} sx={{my: 5}}>
+            <Grid container spacing={2} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                <Grid item xs={12} md={6}>
+                    <Stack direction="row" spacing={2} sx={{alignItems: 'center'}}>
+                        <label htmlFor="raised-button-file">
+                            <IconButton component={'span'} size={'large'}>
+                                <ModelTrainingIcon fontSize="large" sx={{width: '80px', height: '80px', border: '1px solid red'}}/>
+                            </IconButton>
+                        </label>
+                        <Typography variant={'h5'} color={'inherit'} sx={{width: '100%'}}>Choose a model</Typography>
+                    </Stack>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Choose a model</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={model}
+                            label="Choose a model"
+                            onChange={e => setModel(e.target.value)}
+                        >
+                            <>
+                                {models && models.map(modelItem => (
+                                    <MenuItem value={modelItem}>{modelItem}</MenuItem>))}
+                            </>
+                        </Select>
+                    </FormControl>
+                </Grid>
+            </Grid>
         </Container>
+        <hr/>
 
         <Snackbar open={newFileSuccess} autoHideDuration={3000} onClose={closeSnackbar}>
             <AlertCustom onClose={closeSnackbar} severity="success" sx={{width: '100%', mb: 5}}>
