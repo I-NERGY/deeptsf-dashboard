8c70eff1fa3d88eb269f57c7f36d086dd0fe66d3
gkorbakis <gkorbakis@epu.ntua.gr>
2022-09-07 17:10:12 +0300

    Metrics.js additions

--- src/pages/Metrics.js
+++ src/pages/Metrics.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -19,10 +21,21 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Divider from "@mui/material/Divider";
 import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
 
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ModelTrainingIcon from "@mui/icons-material/ModelTraining";
+import DataUsageIcon from '@mui/icons-material/DataUsage';
+import BackspaceOutlinedIcon from '@mui/icons-material/BackspaceOutlined';
 
 import Breadcrumb from "../components/layout/Breadcrumb";
+import ChevronRight from "@mui/icons-material/ChevronRight";
+
 
 ChartJS.register(
     CategoryScale,
@@ -44,7 +57,7 @@ const breadcrumbs = [
         color="secondary"
         fontSize={'20px'}
         fontWeight={600}>
-        <Metrics></Metrics>
+        Metrics
     </Typography>,];
 
 const state = {
@@ -85,15 +98,100 @@ const data = {
 };
 
 const Metrics = () => {
+    const [experiments, setExperiments] = useState([])
+    const [experimentChosen, setExperimentChosen] = useState('')
+    const [experimentChosenError, setExperimentChosenError] = useState(false)
+
+    const [metrics, setMetrics] = useState('')
+    const [metricChosen, setMetricChosen] = useState('')
+
+    const fetchData = () => {
+        setExperimentChosenError(false)
+        !experimentChosen && setExperimentChosenError(true)
+
+        experimentChosen && axios.get('')
+            .then(response => {
+
+            })
+            .catch(error => {
+
+            })
+    }
+
     return (
         <>
             <Breadcrumb breadcrumbs={breadcrumbs} welcome_msg={'Welcome to I-NERGY Load Forecasting'}/>
+
+            <Container maxWidth={'xl'} sx={{my: 5}}>
+                <Typography variant={'h4'} fontWeight={'bold'} sx={{mb: 3}}>Experiment Setup</Typography>
+                <Grid container spacing={2} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                    <Grid item xs={12} md={6}>
+                        <Stack direction="row" spacing={2} sx={{alignItems: 'center'}}>
+                            <ModelTrainingIcon fontSize="large"
+                                               sx={{width: '60px', height: '60px', color: '#A1B927', ml: 2, my: 1}}/>
+                            <Typography variant={'h5'} color={'inherit'} sx={{width: '100%'}}>Experiment</Typography>
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <FormControl fullWidth required error={experimentChosenError}>
+                            <InputLabel id="demo-simple-select-label">Choose an experiment</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={experimentChosen}
+                                error={experimentChosenError}
+                                label="Choose an experiment"
+                                onChange={e => setExperimentChosen(e.target.value)}
+                            >
+                                {experiments && experiments.map(experiment => (
+                                    <MenuItem key={experiment.experiment_id}
+                                              value={experiment.experiment_id}>{experiment.experiment_name}</MenuItem>))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={12} md={6}>
+                        <Stack direction="row" spacing={2} sx={{alignItems: 'center'}}>
+                            <DataUsageIcon fontSize="large"
+                                               sx={{width: '60px', height: '60px', color: '#A1B927', ml: 2, my: 1}}/>
+                            <Typography variant={'h5'} color={'inherit'} sx={{width: '100%'}}>Metric</Typography>
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Choose a metric</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={metricChosen}
+                                disabled={!experimentChosen}
+                                label="Choose an experiment"
+                                onChange={e => setMetricChosen(e.target.value)}
+                            >
+                                {metrics && metrics.map(metric => (
+                                    <MenuItem key={metric.metric_id}
+                                              value={metric.metric_id}>{metric.metric_name}</MenuItem>))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+
+                    <Stack sx={{ml: 'auto', my: 2}} direction={'row'} spacing={2}>
+                        <Button variant={'contained'} component={'span'} size={'large'} color={'success'}
+                                 fullWidth onClick={() => fetchData(experimentChosen, metricChosen)}
+                                endIcon={<ChevronRight/>}>GO</Button>
+                        <Button variant={'contained'} component={'span'} size={'large'} color={'error'}
+                                 fullWidth
+                                endIcon={<BackspaceOutlinedIcon/>}>CLEAR</Button>
+                    </Stack>
+
+                </Grid>
+            </Container>
 
             <Container maxWidth={'xl'} sx={{mt: 5, mb: 2}}>
                 <Grid container direction="row" alignItems="center" justifyItems={'center'}>
                     <Typography variant={'h4'} display={'flex'} alignItems={'center'}>
                         <ChevronRightIcon
-                        fontSize={'large'}/> Model Evaluation Metrics
+                            fontSize={'large'}/> Model Evaluation Metrics
                     </Typography>
                 </Grid>
                 {/*<Typography variant={'h4'}><ChevronRightIcon fontSize={'large'}/> Model Evaluation Metrics</Typography>*/}
@@ -101,16 +199,16 @@ const Metrics = () => {
 
             <Container>
                 <Bar data={state} options={{
-                        title: {
-                            display: true,
-                            text: 'Average Rainfall per month',
-                            fontSize: 20
-                        },
-                        legend: {
-                            display: true,
-                            position: 'right'
-                        }
-                    }}
+                    title: {
+                        display: true,
+                        text: 'Average Rainfall per month',
+                        fontSize: 20
+                    },
+                    legend: {
+                        display: true,
+                        position: 'right'
+                    }
+                }}
                 />
             </Container>
 
