a6bcce0930584a6ece82621d4ca3dfac99f01dac
gkorbakis <gkorbakis@epu.ntua.gr>
2022-09-07 14:09:20 +0300

    Metrics.js additions

--- src/pages/Metrics.js
+++ src/pages/Metrics.js
@@ -7,11 +7,19 @@ import {
     Title,
     Tooltip,
     Legend,
+    PointElement,
+    LineElement,
 } from 'chart.js';
+
 import {Bar} from 'react-chartjs-2';
+import {Line} from 'react-chartjs-2';
 
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Divider from "@mui/material/Divider";
+
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 const state = {
     labels: ['January', 'February', 'March',
@@ -33,20 +41,49 @@ ChartJS.register(
     BarElement,
     Title,
     Tooltip,
-    Legend
+    Legend,
+    PointElement,
+    LineElement,
 );
 
-const Metrics = (props) => {
+const labels = ['January', 'February', 'March',
+    'April', 'May']
+
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: labels.map(() => Math.random() * 100),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            yAxisID: 'y',
+        },
+        {
+            label: 'Dataset 2',
+            data: labels.map(() => Math.random() * 100),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            yAxisID: 'y1',
+        },
+    ],
+};
+
+const Metrics = () => {
     return (
         <>
-            <Container maxWidth={'xl'} sx={{my: 5}}>
-                <h1>METRICS CONTENT</h1>
+            <Container maxWidth={'xl'} sx={{mt: 5, mb: 2}}>
+                <Grid container direction="row" alignItems="center" justifyItems={'center'}>
+                    <Typography variant={'h4'} display={'flex'} alignItems={'center'}>
+                        <ChevronRightIcon
+                        fontSize={'large'}/> Model Evaluation Metrics
+                    </Typography>
+                </Grid>
+                {/*<Typography variant={'h4'}><ChevronRightIcon fontSize={'large'}/> Model Evaluation Metrics</Typography>*/}
             </Container>
 
             <Container>
-                <Bar
-                    data={state}
-                    options={{
+                <Bar data={state} options={{
                         title: {
                             display: true,
                             text: 'Average Rainfall per month',
@@ -59,9 +96,48 @@ const Metrics = (props) => {
                     }}
                 />
             </Container>
+
+            <Divider sx={{my: 5}}/>
+
+            <Container maxWidth={'xl'} sx={{my: 2}}>
+                <Typography variant={'h4'} display={'flex'} alignItems={'center'}>
+                    <ChevronRightIcon
+                        fontSize={'large'}/>Forecasted vs Actual Load Series
+                </Typography>
+            </Container>
+
+            <Container sx={{mb: 5}}>
+                <Line options={{
+                    responsive: true,
+                    interaction: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    stacked: false,
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Chart.js Line Chart - Multi Axis',
+                        },
+                    },
+                    scales: {
+                        y: {
+                            type: 'linear',
+                            display: true,
+                            position: 'left',
+                        },
+                        y1: {
+                            type: 'linear',
+                            display: true,
+                            position: 'right',
+                            grid: {
+                                drawOnChartArea: false,
+                            },
+                        },
+                    },
+                }} data={data}/>
+            </Container>
         </>
-
-
     );
 }
 
