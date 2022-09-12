f78928f3d768708e2dd4cd77146aeb60050c5e7a
gkorbakis <gkorbakis@epu.ntua.gr>
2022-09-12 11:17:54 +0300

    servicesHomepage.js additions

--- src/components/homepage/servicesHomepage.js
+++ src/components/homepage/servicesHomepage.js
@@ -7,33 +7,25 @@ export const servicesHomepage = [
     {
         'id': 'model_training',
         'title': 'Model Training',
-        'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        'description': 'Train your Machine Learning models via a user intuitive UI.',
         'icon': <ModelTrainingIcon style={{color: 'white', fontSize: '80px'}}/>,
         'image': 'https://images.unsplash.com/photo-1639004643331-9526630beb3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1141&q=80',
-        // 'link': '/service/category/ai_services'
+        'link': '/load-forecast'
     },
     {
         'id': 'model_evaluation',
         'title': 'Model Evaluation',
-        'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        'description': 'Evaluate your Machine Learning models and visualise Actual vs Forecasted values.',
         'icon': <RuleIcon style={{color: 'white', fontSize: '80px'}}/>,
         'image': 'https://freerangestock.com/sample/117860/artificial-intelligence--machine-learning--digital-brain-mesh-.jpg',
-        'link': '/service/category/business_cases'
+        'link': '/metrics'
     },
     {
         'id': 'model_deployment',
         'title': 'Model Deployment',
-        'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        'description': 'Deploy your best Machine Learning models.',
         'icon': <TerminalIcon style={{color: 'white', fontSize: '80px'}}/>,
         'image': 'https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        'link': '/service/category/data_analytics'
+        'link': ''
     },
-    // {
-    //     'id': 'advanced_tools',
-    //     'title': 'Advanced Tools',
-    //     'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    //     'icon': <AnalyticsIcon style={{color: 'white', fontSize: '80px'}}/>,
-    //     'image': 'https://images.unsplash.com/photo-1611647832580-377268dba7cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    //     'link': '/service/category/advanced_tools'
-    // },
 ]
