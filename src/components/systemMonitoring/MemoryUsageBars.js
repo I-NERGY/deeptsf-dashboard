deb0150f4aaf0682f750f1036eea15eee9a6c2be
gkorbakis <gkorbakis@epu.ntua.gr>
2022-09-14 17:15:01 +0300

    MemoryUsageBars.js time exceeded fix

--- src/components/systemMonitoring/MemoryUsageBars.js
+++ src/components/systemMonitoring/MemoryUsageBars.js
@@ -13,8 +13,15 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import ProgressBar from "../layout/ProgressBar";
 import Loading from "../layout/Loading";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 const MemoryUsageBars = () => {
+    let memoryCount = 0
+    const liveRefreshMax = 20
+    const [timeExceeded, setTimeExceeded] = useState(false)
+
     const [loading, setLoading] = useState(false)
     const [expanded, setExpanded] = useState(true)
 
@@ -32,22 +39,41 @@ const MemoryUsageBars = () => {
     };
 
     const getMemoryUsageData = () => {
-
         axios.get('/system_monitoring/get_memory_usage')
             .then(response => {
+                console.log(response.data)
+                memoryCount++
                 setVmHigh(response.data.progressbar_1.high)
                 setVmLow(response.data.progressbar_1.low)
-                setVmTitle(response.data.progressbar_1.title) // TODO
+                setVmTitle(response.data.progressbar_1.title)
                 setSwapHigh(response.data.progressbar_2.high)
                 setSwapLow(response.data.progressbar_2.low)
-                setSwapTitle(response.data.progressbar_2.title) // TODO
+                setSwapTitle(response.data.progressbar_2.title)
                 setLoading(false)
                 setMemoryUsageError(false)
             })
             .catch(error => {
+                memoryCount++
                 setMemoryUsageError(true)
                 setLoading(false)
             })
+    }
+
+    const restoreLiveFeed = () => {
+        // Get a reference to the last interval + 1
+        const interval_id = window.setInterval(function () {
+        }, Number.MAX_SAFE_INTEGER);
+
+        // Clear any timeout/interval up to that id
+        for (let i = 1; i < interval_id; i++) {
+            window.clearInterval(i);
+        }
+        setTimeExceeded(false)
+        memoryCount = 0
+        setInterval(() => {
+            memoryCount < liveRefreshMax && getMemoryUsageData()
+            memoryCount >= liveRefreshMax && setTimeExceeded(true)
+        }, 3000)
     }
 
     useEffect(() => {
@@ -55,8 +81,9 @@ const MemoryUsageBars = () => {
         setMemoryUsageError(false)
         getMemoryUsageData()
         setInterval(() => {
-            getMemoryUsageData()
-        }, 60000)
+            memoryCount < liveRefreshMax && getMemoryUsageData()
+            memoryCount >= liveRefreshMax && setTimeExceeded(true)
+        }, 3000)
     }, [])
 
     return (
@@ -74,11 +101,30 @@ const MemoryUsageBars = () => {
                         </Typography>
                     </Grid>
                 </AccordionSummary>
+                {timeExceeded && !loading &&
+                    <AccordionDetails>
+                        <Container>
+                            <Alert severity="info" sx={{alignItems: 'center'}}>
+                                <Typography sx={{width: '100%'}}>
+                                    Live refresh time limit exceeded.
+                                    <Button variant={'contained'} component={'span'} size={'medium'}
+                                            color={'info'}
+                                            sx={{ml: 5}}
+                                            endIcon={<RefreshIcon/>}
+                                            onClick={restoreLiveFeed}>
+                                        Restore live feed
+                                    </Button>
+                                </Typography>
+                            </Alert>
+                        </Container>
+                    </AccordionDetails>
+                }
                 {!loading && !memoryUsageError && <AccordionDetails sx={{my: 4}}>
                     <ProgressBar title={vmTitle} high={vmHigh} low={vmLow}/>
                     <ProgressBar title={swapTitle} high={swapHigh} low={swapLow}/>
                 </AccordionDetails>}
-                {memoryUsageError && !loading && <AccordionDetails><Alert severity="warning" sx={{my: 1}}>No data available.</Alert></AccordionDetails>}
+                {memoryUsageError && !loading && <AccordionDetails><Alert severity="warning" sx={{my: 1}}>No data
+                    available.</Alert></AccordionDetails>}
                 {loading && <AccordionDetails>
                     <Loading/>
                 </AccordionDetails>}
